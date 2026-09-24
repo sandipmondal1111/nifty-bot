f853f44565c4fbc170e33a5977b25fa2eab47f93
sandipmondal1111 <sandipmondal1111@gmail.com>
2026-09-24 22:01:09 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Nifty-Bot Live! Bot is Running"
+    return "Nifty-Bot is LIVE - Running"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
